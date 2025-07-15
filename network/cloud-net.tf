9eb84fc208d86d73034958caed5059c3e3f6a487
Chilet-Okeke <okekechiletaram@gmail.com>
2025-07-15 10:10:22 +0100

    instances

--- network/cloud-net.tf
+++ network/cloud-net.tf
@@ -19,6 +19,26 @@ resource "google_compute_subnetwork" "Migra-subnet2" {
     region = "var.region"
     network = google_compute_network.Migra-net.id
     description = "migra_subnet-2_for_onprem-cloud_peering"
+}
 
-  
+resource "google_compute_firewall" "Migra-firewall-allow-egress" {
+name = "${var.network_name}--firewall-allow-egress"
+network = google_compute_network.Migra-net.id
+allow {
+  protocol =  "var.firewall_egress_protocol"   #default "tcp"
+  ports = "var.firwall_custom_ports"   #default ["80", "443"]
+}
+source_ranges = "var.firewall_custom_source_ranges" #["0.0.0.0/0"]
+description = "migra_firewall_allow_custom"
+}
+
+resource "google_compute_firewall" "name" {
+  name = "${var.network_name}--firewall-allow-ingress"
+  network = google_compute_network.Migra-net.id
+  allow {
+    protocol = "var.firewall_ssh-icmp_protocol" #default "tcp"
+    ports = "var.firewall_ssh-icmp_ports" #default "80"
+  }
+  source_ranges = "var.firewall_ssh-icmp_source_ranges" #default "10.0.0.0/8"
+  description = "migra_firewall_allow_ssh-icmp"
 }
